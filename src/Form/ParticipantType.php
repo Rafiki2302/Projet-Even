a8ecd96bc31ebd2177578a541c07c1b45b2b5914
Malko <thomas.lebars2019@campus-eni.fr>
2019-08-27 12:27:31 +0200

    Créations des Entités Participant et Site + controllers, form et vues associées

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -6,6 +6,7 @@ use App\Entity\Participant;
 use App\Entity\Site;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
