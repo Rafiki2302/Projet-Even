8f066d325597251fd52f0f82ba7e9a3e193667e2
vroch2019 <valerie.roch2019@campus-eni.fr>
2019-09-04 12:04:41 +0200

    init V new

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 
 /**
@@ -31,6 +32,7 @@ class Sortie
      * @Assert\NotBlank(message="Le champ doit être rempli !")
      * @var string A "Y-m-d" formatted value
      * @Assert\GreaterThan("today", message="La date de début doit être supérieure à la date actuelle")
+     *
      */
     private $datedebut;
 
@@ -47,6 +49,7 @@ class Sortie
      * @Assert\DateTime(message="Format de date invalide")
      * @var string A "Y-m-d" formatted value
      * @Assert\GreaterThan("today", message="La date de clôture doit être supérieure à la date actuelle")
+     * @Assert\Expression("value < this.getDatedebut()", message= "cette date doit être antérieure à celle de l'évènement")
      */
     private $datecloture;
 
